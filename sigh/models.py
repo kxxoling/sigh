1c850e51f21edb48ab6e102f9bb941ee83373165
Kane Blueriver <kxxoling@gmail.com>
2015-05-21 22:37:26 +0800

    Add tags model

--- sigh/models.py
+++ sigh/models.py
@@ -27,14 +27,30 @@ class User(BasicModel, db.Model):
     family_name = db.Column(db.String(100))
     register_time = db.Column(db.DateTime)
     role = db.Column(db.Integer)
-    sigh = db.relationship('Sigh')
+    sighs = db.relationship('Sigh')
+    tags = db.relationship('Tag')
+
+
+tag_identifier = db.Table('tag_identifier',
+                          db.Column('tag_id', db.Integer, db.ForeignKey('tags.id_')),
+                          db.Column('sigh_id', db.Integer, db.ForeignKey('sighs.id_')))
 
 
 class Sigh(BasicModel, db.Model):
     __tablename__ = 'sighs'
 
-    creater_id = db.Column(db.Integer, db.ForeignKey('users.id_'))
+    creator_id = db.Column(db.Integer, db.ForeignKey('users.id_'))
     create_time = db.Column(db.DateTime)
     content = db.Column(db.Text)
     type_ = db.Column(db.Enum('sigh', 'wtf', 'fml'))
     is_anonymous = db.Column(db.Boolean)
+    tags = db.relationship('Tag', secondary=tag_identifier)
+
+
+class Tag(BasicModel, db.Model):
+    __tablename__ = 'tags'
+
+    name = db.Column(db.String(50), unique=True, nullable=False)
+    display_name = db.Column(db.String(50), unique=True, nullable=False)
+    creator_id = db.Column(db.Integer, db.ForeignKey('users.id_'))
+    create_time = db.Column(db.DateTime)
