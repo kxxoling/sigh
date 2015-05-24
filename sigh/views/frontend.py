fff105a27414b5d89c049d065bb1560f81147dae
Kane Blueriver <kxxoling@gmail.com>
2015-05-24 21:46:17 +0800

    Add some site data to side bar

--- sigh/views/frontend.py
+++ sigh/views/frontend.py
@@ -1,13 +1,24 @@
 from flask import Blueprint
 from flask import render_template, jsonify
 from flask import url_for, request
+from flask import g
 
 from ..models import Sigh
 from ..forms import SighForm, CommentForm
 from ..models import Comment
+from ..models import User
+from ..models import Tag
 
 
 frontend_views = Blueprint('frontend', __name__, url_prefix='/')
+
+
+@frontend_views.before_request
+def get_site_info():
+    g.user_count = User.query.count()
+    g.comment_count = Comment.query.count()
+    g.sigh_count = Sigh.query.count()
+    g.tag_count = Tag.query.count()
 
 
 @frontend_views.route('/')
