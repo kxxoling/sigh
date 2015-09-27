82b00108ec1c7f315a755ca675c8fd26d4086f4f
kxxoling <kxxoling@gmail.com>
2015-09-27 22:15:35 +0800

    Add Flask-DebugToolbar plugin in debug mode

--- run.py
+++ run.py
@@ -1,14 +1,19 @@
 from __future__ import print_function
 import os
 from sigh.apps import create_app
+from flask.ext.debugtoolbar import DebugToolbarExtension
 
 
-config_file = os.path.join(os.path.dirname(os.path.realpath(__file__))
-                           , 'config.py')
-app = create_app(config_file)
+config_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'config.py')
+
+
+def debug_app(app):
+    app.debug = True
+    toolbar = DebugToolbarExtension()
+    toolbar.init_app(app)
 
 
 if __name__ == '__main__':
-    print('URL map:')
-    print(app.url_map)
-    app.run(debug=True, host='0.0.0.0')
+    application = create_app(config_file)
+    debug_app(application)
+    application.run(debug=True, host='0.0.0.0')
