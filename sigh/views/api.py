98925a82dfb45a4c76496cd11af8d1483a678e6e
kxxoling <kxxoling@gmail.com>
2015-10-04 15:07:06 +0800

    Create a new API for User autocompletion

--- sigh/views/api.py
+++ sigh/views/api.py
@@ -5,6 +5,7 @@ from flask import Blueprint
 from flask import Response
 
 from ..models import Tag
+from ..models import User
 
 
 api_views = Blueprint('api', __name__, url_prefix='/api/')
@@ -24,3 +25,11 @@ def autocomplete_tag(q):
     tags = Tag.query.filter(Tag.searchable_name.ilike(u'%{}%'.format(q.lower()))).all()
     tags = [tag.to_dict('id_', 'display_name') for tag in tags]
     return tags
+
+
+@api_views.route('user/autocompletion/<q>')
+@jsonify
+def autocomplete_user(q):
+    users = User.query.filter(User.username.ilike(u'%{}%'.format(q.lower()))).all()
+    users = [user.to_dict('id_', 'name', 'username', 'avatar') for user in users]
+    return users
