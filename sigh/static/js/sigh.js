339a8b2b11ff48721d6bf2dba41311c9351b28d3
kxxoling <kxxoling@gmail.com>
2015-12-05 12:37:19 +0800

    Fix comment JS with new HTML structure

--- sigh/static/js/sigh.js
+++ sigh/static/js/sigh.js
@@ -3,14 +3,38 @@ $().ready(function () {
   $("form.comment").on("submit", function (event) {
     event.preventDefault();
     var $form = $(this);
-    var $comments = $(".comment-group");
+    var $commentTableFirstColumn = $("table.comment tr:first");
     var $input = $("textarea.comment");
     $.ajax({
       type: $form.attr("method"),
       url: $form.attr("action"),
       data: $form.serialize(),
       success: function (e, status) {
-        $comments.prepend("<p>"+e.content+"</p>");
+        var image = '',
+            username = '',
+            fullname = '',
+            content = '';
+        $row = '<tr>\
+  <td class="left">\
+    <div style="background-image: url("'
+        + image + '")" class="avatar"></div>\
+  </td>\
+  <td class="right">\
+    <div class="relate-info clearfix">\
+      <div class="pull-left"><a data-username="'
+        + username + '" href="/u/1/" class="name">'
+        + fullname + '</a>\
+      </div>\
+      <div class="pull-right"><span class="time">Just now</span>\
+      </div>\
+    </div>\
+    <div class="content">\
+      <p>'
+        + content + '</p>\
+    </div>\
+  </td>\
+</tr>';
+        $commentTableFirstColumn.before($row);
         $input.val("");
       },
       error: function (e, status) {
