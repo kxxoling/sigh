0ff9c5464d03844784da44c79481dec491db5fe1
kxxoling <kxxoling@gmail.com>
2015-12-05 12:38:56 +0800

    Replace tabs with spaces

--- sigh/utils.py
+++ sigh/utils.py
@@ -4,23 +4,24 @@ from flask.ext.babel import gettext as _
 
 
 def timeago(dt):
-	now = datetime.datetime.now()
-	delta = now - dt
-	if delta < datetime.timedelta(minutes=2):
-		return _('Just now')
-	if delta < datetime.timedelta(hours=1):
-		return _('{} minutes ago').format(delta.seconds / 60)
-	if delta < datetime.timedelta(hours=2):
-		return _('1 hour ago')
-	if delta < datetime.timedelta(days=1):
-		return _('{} hours ago').format(delta.seconds / 3600)
-	if delta < datetime.timedelta(days=2):
-		return _('1 day ago')
-	return _('{} days ago').format(delta.days)
+    now = datetime.datetime.now()
+    delta = now - dt
+    print delta
+    if delta < datetime.timedelta(minutes=2):
+        return _('Just now')
+    if delta < datetime.timedelta(hours=1):
+        return _('{} minutes ago').format(delta.seconds / 60)
+    if delta < datetime.timedelta(hours=2):
+        return _('1 hour ago')
+    if delta < datetime.timedelta(days=1):
+        return _('{} hours ago').format(delta.seconds / 3600)
+    if delta < datetime.timedelta(days=2):
+        return _('1 day ago')
+    return _('{} days ago').format(delta.days)
 
 
 def plain_markdown(text):
-	renderer = misaka.HtmlRenderer(flags=misaka.HTML_ESCAPE)
-	md = misaka.Markdown(renderer)
-	return md.render(text)
+    renderer = misaka.HtmlRenderer(flags=misaka.HTML_ESCAPE)
+    md = misaka.Markdown(renderer)
+    return md.render(text)
 
