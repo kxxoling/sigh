fc0cf9cc692b04e2067079c02adc910bebcb65ce
kxxoling <kxxoling@gmail.com>
2016-01-29 12:12:58 +0800

    Refactor comment list from table view to div;

--- sigh/static/js/sigh.js
+++ sigh/static/js/sigh.js
@@ -3,45 +3,40 @@ $().ready(function () {
   $("form.comment").on("submit", function (event) {
     event.preventDefault();
     var $form = $(this);
-    var $commentTableFirstColumn = $("table.comment tr:first");
+    var $commentTableFirstColumn = $(".comment.list .list-item:first");
     var $input = $("textarea.comment");
-    $.ajax({
-      type: $form.attr("method"),
-      url: $form.attr("action"),
-      data: $form.serialize(),
-      success: function (e, status) {
-        var image = '',
-            username = '',
-            fullname = '',
-            content = '';
-        $row = '<tr>\
-  <td class="left">\
-    <div style="background-image: url("'
-        + image + '")" class="avatar"></div>\
-  </td>\
-  <td class="right">\
-    <div class="relate-info clearfix">\
-      <div class="pull-left"><a data-username="'
-        + username + '" href="/u/1/" class="name">'
-        + fullname + '</a>\
-      </div>\
-      <div class="pull-right"><span class="time">Just now</span>\
-      </div>\
-    </div>\
-    <div class="content">\
-      <p>'
-        + content + '</p>\
-    </div>\
-  </td>\
-</tr>';
-        $commentTableFirstColumn.before($row);
+    $.post($form.attr("action"), $form.serialize(), function (data, status) {
+        var $user = $('.comment-form .avatar')
+        var image = $user.data('avatar'),
+            username = $user.data('username'),
+            fullname = $user.data('name'),
+            id=$user.data('id'),
+            content = data.content;
+
+        var rowStr = '<div class="list-item">\
+<div class="list-left">\
+<div style="background-image: url('+ image +')" class="avatar"></div>\
+</div>\
+<div class="list-right">\
+<div class="relate-info clearfix">\
+<div class="pull-left"><a data-username="' + username + '" href="/u/+ id +/" class="name">' + fullname +'</a>\
+</div>\
+<div class="pull-right"><span class="time">' + 'Just now' + '</span>\
+</div>\
+</div>\
+<div class="content"><p>'+ content + '</p></div>\
+</div>\
+</div>'
+        $commentTableFirstColumn.before(rowStr);
         $input.val("");
-      },
-      error: function (e, status) {
+      }, 'json').fail(function (e, status) {
         var errors = e.responseJSON;
-        $form.find(".content").addClass("has-error").find("span.tip").text(errors.content[0])
-      }
-    });
+        console.log(errors);
+        $form.addClass("has-error");
+        setTimeout(function () {
+          $form.removeClass('has-error')
+        }, 2000);
+      });
     return false;
   });
 
